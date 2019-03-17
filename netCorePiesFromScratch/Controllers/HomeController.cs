3681c30b40f416e917a458a3b1e809f9fce0d439
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2019-03-17 12:51:14 +0100

    Added Controller.

---
+++ netCorePiesFromScratch/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netCorePiesFromScratch.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace netCorePiesFromScratch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        // This is DI - since it is defined in startup class, this will auto create the repository
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
