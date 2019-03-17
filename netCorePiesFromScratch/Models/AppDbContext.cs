70cd11b94f0d51be280dfd04ebfa0c2613311482
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2019-03-17 19:22:02 +0100

    Added dbContext for pies.

---
+++ netCorePiesFromScratch/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace netCorePiesFromScratch.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
--- netCorePiesFromScratch/Startup.cs
+++ netCorePiesFromScratch/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using netCorePiesFromScratch.Models;
 
@@ -7,11 +9,20 @@ namespace netCorePiesFromScratch
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get;}
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IPieRepository, PieRepository>();
             services.AddMvc();
         }
 
