d1af207e05f44523a2635263264e8a2b2b03a001
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2019-03-17 12:45:18 +0100

    Created interface for pie repository and mock pie repository.

---
+++ netCorePiesFromScratch/Models/IPieRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace netCorePiesFromScratch.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
